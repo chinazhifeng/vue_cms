5a849c18a55af9e1cbea7cddec073f498fb1908d
zhangmrad <5607361+zhangmrad@user.noreply.gitee.com>
2020-04-28 16:04:46 +0800

    项目第一天

--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
 import Vue from 'vue'
+// 1.1 导入路由的包
+import VueRouter from 'vue-router'
+// 1.2 安装路由
+Vue.use(VueRouter)
+
+//导入vue-resource
+import VueResource from 'vue-resource'
+//安装vue-resource
+Vue.use(VueResource)
 
 //按需导入mint-ui组件
 import 'mint-ui/lib/style.css'
-import { Header } from 'mint-ui';
+import { Header,Swipe, SwipeItem  } from 'mint-ui';
 
 //导入min-ui的样式
 import './lib/mui/css/mui.css'
 
-Vue.component(Header.name, Header);
+import './lib/mui/css/icons-extra.css'
 
+Vue.component(Header.name, Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+import router from './router.js'
 
 //导入APP根组件
 import app from './App.vue'
@@ -18,5 +32,6 @@ var vm = new Vue({
     data:{
 
     },
-    render:c=>c(app)
+    render:c=>c(app),
+    router
 });
--- src/router.js
+++ src/router.js
@@ -1,13 +1,20 @@
 import VueRouter from 'vue-router'
 
-
+//导入对应的路由组件
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import ShopContainer from './components/tabbar/ShopContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
 
 var router =new VueRouter({
     routes:[
-        {
-           
-        }
-    ]
+           {path:'/' ,redirect:'/home'},
+           {path:'/home' ,component:HomeContainer},
+           {path:'/member' ,component:MemberContainer},
+           {path:'/shop' ,component:ShopContainer},
+           {path:'/search' ,component:SearchContainer}
+    ],
+    linkActiveClass: 'mui-active'
 })
 
 export default router
