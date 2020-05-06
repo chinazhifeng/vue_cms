ba59f4a1572322a13f18469b6e6e2f24175ce82c
zhangmrad <5607361+zhangmrad@user.noreply.gitee.com>
2020-05-06 16:20:14 +0800

    2020/05/06第一次提交

--- src/main.js
+++ src/main.js
@@ -4,14 +4,25 @@ import VueRouter from 'vue-router'
 // 1.2 安装路由
 Vue.use(VueRouter)
 
+//导入格式化时间的插件
+import moment from 'moment'
+
+//定义全局的过滤器
+Vue.filter('dateFormat',function(dataStr,pattern="YYYY-MM-DD HH:mm:ss"){
+    return moment(dataStr).format(pattern)
+})
+
 //导入vue-resource
 import VueResource from 'vue-resource'
 //安装vue-resource
 Vue.use(VueResource)
 
+//设置请求的根路径
+Vue.http.options.root = 'http://www.liulongbin.top:3005';
+
 //按需导入mint-ui组件
 import 'mint-ui/lib/style.css'
-import { Header,Swipe, SwipeItem  } from 'mint-ui';
+import { Header,Swipe, SwipeItem,Button  } from 'mint-ui';
 
 //导入min-ui的样式
 import './lib/mui/css/mui.css'
@@ -21,7 +32,7 @@ import './lib/mui/css/icons-extra.css'
 Vue.component(Header.name, Header);
 Vue.component(Swipe.name, Swipe);
 Vue.component(SwipeItem.name, SwipeItem);
-
+Vue.component(Button.name, Button);
 import router from './router.js'
 
 //导入APP根组件
--- src/router.js
+++ src/router.js
@@ -5,14 +5,17 @@ import HomeContainer from './components/tabbar/HomeContainer.vue'
 import MemberContainer from './components/tabbar/MemberContainer.vue'
 import ShopContainer from './components/tabbar/ShopContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
-
+import NewsList from './components/news/NewsList.vue'
+import NewsInfo from './components/news/NewsInfo.vue'
 var router =new VueRouter({
     routes:[
            {path:'/' ,redirect:'/home'},
            {path:'/home' ,component:HomeContainer},
            {path:'/member' ,component:MemberContainer},
            {path:'/shop' ,component:ShopContainer},
-           {path:'/search' ,component:SearchContainer}
+           {path:'/search' ,component:SearchContainer},
+           {path:'/home/newsList',component:NewsList},
+           {path:'/home/newsinfo/:id',component:NewsInfo}
     ],
     linkActiveClass: 'mui-active'
 })
